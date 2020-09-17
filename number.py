99091386699ac9e20b203bb063f7090acba0c2d6
Tina Kim <kimm71@student.douglascollege.ca>
2020-09-17 22:17:37 +0900

    number and random function study

---
+++ number.py
@@ -0,0 +1,56 @@
+print(abs(-5)) # absolute value
+print(pow(4, 2)) #16
+print(max(5, 12))
+print(min(5, 12))
+print(round(3.14))
+print(round(4.99))
+print("=" * 20)
+
+from math import *
+print(floor(4.99)) #4
+print(ceil(3.14)) #4
+print(sqrt(16)) #4.0
+print("=" * 20)
+
+from random import *
+print(random())
+print(random() * 10)
+
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10))
+
+print(int(random() * 100))
+print(int(random() * 100))
+print(int(random() * 100))
+
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+print("=" * 20)
+
+# 로또 번호 출력
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print("=" * 20)
+# Rand range()
+print(randrange(1, 46)) # 1 ~ 45
+print(randrange(1, 46))
+print(randrange(1, 46))
+print(randrange(1, 46))
+print(randrange(1, 46))
+print(randrange(1, 46))
+print("=" * 20)
+
+#
+print(randint(1, 45)) #1부터 45까지 모두 포함
+print(randint(1, 45)) 
+print(randint(1, 45)) 
+print(randint(1, 45)) 
+print(randint(1, 45)) 
+print(randint(1, 45)) 
